26ca16e1c89c5bae47739b74f11e774433b3f734
Amelia Sundman <sundman@bu.edu>
2022-11-07 16:59:55 -0500

    fixed template bug

--- prototype/views.py
+++ prototype/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'prototype/post_list.html', {})
+    #i changed this to say prototype instead of blog because my template is looking for blog not prototype
     
