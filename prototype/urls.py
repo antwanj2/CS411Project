7f8935a86060f8cbd49cee6997275e077af50d26
Amelia Sundman <sundman@bu.edu>
2022-11-08 22:01:07 -0500

    how do we call the zip function and pass in a zipcode

--- prototype/urls.py
+++ prototype/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
     path('', views.zip, name='zip'), #i think zip is the variable name ill call it
 ]
--- prototype/views.py
+++ prototype/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+import json
 from apikeys import openweather_key
 
 # Create your views here.
@@ -8,13 +9,14 @@ def post_list(request):
 
 def zip(request): #function to turn zipcodes into lat/lon? idk how to test it yet it does not work
     user_zip = request.META.get('wtf idk what this means', '')
-    response = requests.get('http://api.openweathermap.org/geo/1.0/zip?zip=%s,US&appid=%s' % user_zip % openweather_key)
-    zipdata = response.json()
+    response = requests.get('http://api.openweathermap.org/geo/1.0/zip?zip=%s,US&appid=d29579363a4d3c6b0eb89de9f488eb3c' % user_zip)
+    zipdata = response.text
+    zipdatajson = json.loads(zipdata)
     return render(request, 'prototype/post_list.html', {
-  "zip": zipdata['zip'],
-  "name": zipdata['name'],
-  "lat": zipdata['lat'],
-  "lon": zipdata['lon'],
-  "country": zipdata['country']
+  'zip': zipdatajson['zip'],
+  'name': zipdatajson['name'],
+  'lat': zipdatajson['lat'],
+  'lon': zipdatajson['lon'],
+  'country': zipdatajson['country']
 })
     
