e76451ee3632cf9309d39e1eb0fbdcea741c87fe
Maria Shevchuk <87496627+mariashev@users.noreply.github.com>
2022-11-08 22:41:25 -0500

    update method to post

--- prototype/views.py
+++ prototype/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 import requests
 import json
 from apikeys import openweather_key
-from flask import Flask 
-from flask import request 
  
 
 # Create your views here.
@@ -11,7 +9,8 @@ def post_list(request):
     return render(request, 'prototype/post_list.html', {}) #i changed this to say prototype instead of blog because my template is looking for blog not prototype
 
 def zip(request): #function to turn zipcodes into lat/lon? idk how to test it yet it does not work
-    user_zip = request.form['zipcode'] #this is getting the user input from html
+    if request.method == 'POST':
+       user_zip = request.POST.get('textfield', None)
     response = requests.get('http://api.openweathermap.org/geo/1.0/zip?zip=%s,US&appid=d29579363a4d3c6b0eb89de9f488eb3c' % user_zip)
     zipdata = response.text
     zipdatajson = json.loads(zipdata)
