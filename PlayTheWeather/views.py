7d80837021d8d9986b3be8e78437f2cdafc3c852
Amelia Sundman <sundman@bu.edu>
2022-11-11 13:13:02 -0500

    added render function to views

--- PlayTheWeather/views.py
+++ PlayTheWeather/views.py
@@ -10,23 +10,23 @@ def homepage(request):
 #check whether or not the user is signed in
 # if they're signed in forward them to the zipcode page
 #if they're not display a page that will describe our app and prompt them to log in
-    return
+    return render(request, 'PlayTheWeather/homepage.html')
 
 #zipcode view
 def zipcode(request):
 #if they have entered a zipcode before ask if they wanna keep it or change
 #if not just ask to enter a zipcode
 #then hit a button that will open a new view with the generate button playlist
-    return
+    return render(request, 'PlayTheWeather/zipcode.html')
 
 #generate button playlist view
 def playlist(request):
 #display weather
 #call spotify API here and then open up the link to the spotify playlist
-    return
+    return render(request, 'PlayTheWeather/playlist.html')
 
 #settings view
-def settings(requst):
+def settings(request):
 #not sure what settings we will have here but i bet we will need em
 #definitely the option to log out
-    return
+    return render(request, 'PlayTheWeather/settings.html')
