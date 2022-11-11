a5a2cafc11986d23ba4ff518c3cb8c2837e1ff75
Amelia Sundman <sundman@bu.edu>
2022-11-11 12:57:21 -0500

    added fields to spotify user model

--- PlayTheWeather/models.py
+++ PlayTheWeather/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 # Create your models here.
 
 #database for the spotify users?
-#fields: username
-#latest zipcode
-#???
+class Spotify_User(models.Model):
+    username = models.CharField(max_length=200)
+    latest_zipcode = models.IntegerField()
+    #???
+    def __str__(self):
+        return self.username
