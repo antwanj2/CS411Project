378faf5acddc22689e61d12d58f0227cf45e99c3
Amelia Sundman <sundman@bu.edu>
2022-11-11 13:36:28 -0500

    more specific comments for homepage view

--- PlayTheWeather/views.py
+++ PlayTheWeather/views.py
@@ -10,12 +10,14 @@ def homepage(request):
 #check whether or not the user is signed in
 # if they're signed in forward them to the zipcode page
 #if they're not display a page that will describe our app and prompt them to log in
+#if they have never signed in before add them to the database with null as their zipcode
+#once they log in direct them to the zipcode page
     return render(request, 'PlayTheWeather/homepage.html')
 
 #zipcode view
 def zipcode(request):
 #if they have entered a zipcode before ask if they wanna keep it or change
-#if not just ask to enter a zipcode
+#if not (zipcode == null) just ask to enter a zipcode
 #then hit a button that will open a new view with the generate button playlist
     return render(request, 'PlayTheWeather/zipcode.html')
 
