bb51e6e6e90e2753ef97bcd59c41d71c2c74a1ce
Amelia Sundman <sundman@bu.edu>
2022-12-01 17:36:09 -0500

    I'm learning how the spotify API works rn

--- PlayTheWeather/models.py
+++ PlayTheWeather/models.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 class Spotify_User(models.Model):
     username = models.CharField(max_length=200)
     latest_zipcode = models.IntegerField()
-    #???
+    access_token = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=200)
     def __str__(self):
         return self.username
