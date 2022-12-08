5003a278a122f5e0faca1816481a54a3d593729a
AmeliaSund <sundman@bu.edu>
2022-12-07 22:49:42 -0500

    view for playlist if we can't use spotify api

--- PlayTheWeather/views.py
+++ PlayTheWeather/views.py
@@ -66,6 +66,21 @@ def playlist(request):
     
     return render(json_resp, 'PlayTheWeather/playlist.html')
 
+def no_api_playlist(request):
+    #I used official spotify playlists for all of the general weather conditions
+    if int(weather_id) >= 800: #cloudy/clear
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1DX1BzILRveYHb?si=525261fed6aa4496'
+    elif int(weather_id) >= 700: #random atmosphere
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1EIgdrHtsTSmGq?si=9120e366c25242c6'
+    elif int(weather_id) >= 600: #snow
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1DX64iwDddhmfW?si=655e2968a213472b'
+    elif int(weather_id) >= 500: #rain
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1EIelivQWnxTte?si=07d0b268ef894ab3'
+    elif int(weather_id) >= 300: #drizzle
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1DX2mFmJUZg4Mp?si=fdbf5e472cfa4058'
+    else: #thunder
+        playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1DWXKbJeFbii64?si=8df0d8fa5f664b15'
+    return render(request, 'PlayTheWeather/playlist.html', {playlist_url})
 #settings view
 def settings(request):
 #not sure what settings we will have here but i bet we will need em
