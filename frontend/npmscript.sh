58bd014633ac72ff0983061c8d0faecc6c64ed08
Antwan <antwanj2@bu.edu>
2022-12-08 19:44:20 -0500

    minor modification of script

--- frontend/npmscript.sh
+++ frontend/npmscript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 npm init -y
 npm i webpack webpack-cli --save-dev
-npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
+sudo npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
 npm i react react-dom --save-dev
 yarn add @material-ui/core
-npm install @babel/plugin-proposal-class-properties
+sudo npm install @babel/plugin-proposal-class-properties
 npm install react-router-dom
 yarn add @material-ui/icons
